f3a3c110414d4cbe436f054552929b0005d7fc91
benita sien <benitasien@gmail.com>
2022-10-21 19:08:09 +1100

    add species info page

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function search_pets() {
     let input = document.getElementById('page-searchbar').value;
-
     input=input.toLowerCase();
     let x = document.getElementsByClassName('species');
-      
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
